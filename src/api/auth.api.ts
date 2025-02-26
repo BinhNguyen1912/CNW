95d7e30b50a583a94028770893205a41a778e7e6
Binh Nguyen <anhbinhnguyen19122004@gmail.com>
2025-02-26 07:34:13 +0700

    update src/api/athu.api.ts

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -2,5 +2,5 @@ import { authRespone } from 'src/types/auth.type'
 import http from 'src/Until/Http'
 const auth = '/auth'
 export const registerByUser = (body: { email: string; password: string; phone: string; name: string }) => {
-  return http.post<authRespone>(auth + '/registerByUserr', body)
+  return http.post<authRespone>(auth + '/registerByUser', body)
 }
